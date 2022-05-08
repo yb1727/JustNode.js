43debee9538631eb332e59ad88ccde085a39d2dd
paco raban <paco@raban.com>
2022-05-08 17:06:30 -0500

    Build: Adding ts configuration to compile all ts files in src and place the resulting js fileson compiledJS direcotry

--- compiledJS/first-app.js
+++ compiledJS/first-app.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 fs.writeFileSync("hello.txt", "Hello World");
 console.log('written to file : Hello World');
--- src/first-app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var fs = require("fs");
-fs.writeFileSync("hello.txt", "Hello World");
-console.log('written to file : Hello World');
