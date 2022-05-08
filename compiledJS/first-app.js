6841b4625174604c6f46dc39b9ae4bdbf2341c5f
paco raban <paco@raban.com>
2022-05-08 15:44:47 -0500

    Initial repo: added a siple fle writing command and scrips in package.json

---
+++ compiledJS/first-app.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+fs.writeFileSync("hello.txt", "Hello World");
+console.log('written to file : Hello World');
---
+++ src/first-app.ts
@@ -0,0 +1,5 @@
+import * as fs from "fs";
+
+fs.writeFileSync("hello.txt", "Hello World");
+
+console.log ('written to file : Hello World');
