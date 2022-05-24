20218cb9e2971ec0cb94e2f0aedd558b99149c51
paco raban <paco@raban.com>
2022-05-24 13:50:34 -0500

    Chapter 24.3 - REST API - Adding CORS configuration to allow clients hosted on domains, to be able to acccess this app, without any CORS errors.

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,19 @@ app.use(bodyParser.json());
 // 24.1.7 - After building the feed router, we will first need to import it
 const feedRoute = require ('./routes/feed');
 
+//24.3 CORS : when a client is hosted on a domain, we need to configure the middleware to allow requests from such clients (e.g. SPA clients)
+app.use((req,res,next) => {
+    // Allow all client origin domain and port. instead of '*' you can also specify a single client domain or comma separated list of domains with ports they are using (e.g. 'myspaapp.com:3000, blueapp.org')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Allow client to send http methods. you can also specify a single method or comma separated list of methods (e.g. 'GET,PUT,POST,DELETE')
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    // Allow client to send only Content-Type or Authorization headers. If you want to allow more headers, add to the list, or change the list to '*' to allow all headers.
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    // Continue to the next middleware in this chain (line 11)
+    next();
+
+});
+
 // 24.1.8 Here, we are assigning all requests received on the /feed route to be handled by feeRoute router.
 //     if we omit the path, all paths will be sent to thar router
 app.use('/feed', feedRoute);
