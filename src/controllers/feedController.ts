d0ff493ed31209476987625771c1b0c0928ba3b1
paco raban <paco@raban.com>
2022-05-24 21:35:14 -0500

    Chapter 25.1 - adding POST request dada validation with express-validator

--- src/controllers/feedController.ts
+++ src/controllers/feedController.ts
@@ -1,13 +1,37 @@
+// 25.1.3 - To be able to validate the request body using the rules sent by the router
+//          we will first import the validator check from express-validator package.
+const {validationResult} = require ('express-validator/check');
+
 // 24.1.9 Once this controller method reference is called form the router, we can then return a response in a json format.
 exports.getPosts= (req,res, next) => {
   //   24.1.10 we will return a status and a response. The default is 200 so the status can be omitted, but it was added here
     // to show that option , and soon we will return codes different than 200.
   res.status(200).json({
-      posts: [{title: 'first post', content: 'This is the first post'}]
+      posts: [{
+               _id: '1',
+               title: 'first post',
+               content: 'This is the first post',
+               imageUrl: 'images/duck.jpg',
+               creator: {
+                    name: 'Yair'
+               },
+               createdAt: new Date()
+      }]
   });
 };
 
 exports.createPosts = (req,res, next) => {
+    // 25.1.4 - Now, before processing the incoming post request, we will perform validation
+    //             which were sent by the router, and return an error status and message , if validations failed
+    const errors = validationResult(req);
+    if (! errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({
+                message: 'request validations failed',
+                errors: errors.array()
+            });
+    }
     // 24.2.3 : We are expecting the client to send a request with json body which has a title and content fields.
     //          So, here we are extracting that information from the body. Since the body parser was already configured
     //          to parse json format of request bodies, these should be available here.
@@ -18,6 +42,11 @@ exports.createPosts = (req,res, next) => {
     //            and populating the response body with data from the request body.
     res.status(201).json({
         message : "Data was created successfully",
-        post: {id: new Date().toISOString(),title: title, content: content}
+        post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: {name: 'Yair'}},
+            createdAT: new Date()
     });
 };
--- src/routes/feed.ts
+++ src/routes/feed.ts
@@ -1,5 +1,8 @@
 import * as express from 'express';
 
+// 25.1.1 - to perform validations on input data, we will use the express validator
+const {body} = require ('express-validator/check');
+
 // 24.1.3 In this file we will store our router which invokes a controller method based on the incoming request http verb
 //      (GET, POST, PUT, DELETE) and path.
 
@@ -18,6 +21,13 @@ router.get('/posts', feedController.getPosts);
 // 24.2.5 Adding a router for a POST method.
 //        Here, app.ts will send all requests on /feed to this router, to be able to match this router mapping,
 //        the POST request must be sent by the client on path /feed/post
-router.post('/post', feedController.createPosts);
+router.post('/post',
+// 25.1.2 - as par tof this router for POST, we are adding requirements using the 'body' validator we imported, require
+//          that title and content will be atleat 5 characters long. Validating the request body based on these rules will be done in the controller itself.
+    [
+        body('title').trim().isLength(({min: 5})),
+        body('content').trim().isLength(({min: 5})),
+    ],
+    feedController.createPosts);
 
 module.exports = router;
